5bf31585ff741ada00a27911e579a3b7e86dd1dd
Renzo Guerra <renzoguerralatt@gmail.com>
2026-05-30 17:15:50 -0300

    ADD: Mensaje de antelacion a la demora de ejecucion temprana

--- src/pages/LogInPage/LogInPage.tsx
+++ src/pages/LogInPage/LogInPage.tsx
@@ -5,6 +5,13 @@ export const LogInPage = () => {
   return (
     <main className="page-container">
       <section className="login-content">
+        <div>
+          <p>Leer</p>
+          <p>Debido a que el backend está hosteado en una plataforma gratuita, la primera petición (loguearse) demora aproximadamente 1 minuto.</p>
+          <p>Una vez establecida la conexión las peticiones duran milisegundos.</p>
+          <p>Futuramente pagaré un host para solucionar este problema.</p>
+          <p>Sepan disculparme.</p>
+        </div>
         <FormLogIn />
       </section>
     </main>
